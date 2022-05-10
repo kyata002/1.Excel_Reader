887e442167fdc375aff29781d5bebf7a0145d137
1451020071@dnu.edu.vn <1451020071@dnu.edu.vn>
2022-05-10 11:49:25 +0700

    add

--- app/src/main/java/com/masterlibs/basestructure/view/activity/SplashActivity.kt
+++ app/src/main/java/com/masterlibs/basestructure/view/activity/SplashActivity.kt
@@ -8,8 +8,11 @@ import com.masterlibs.basestructure.R
 
 class SplashActivity(override val layoutId: Int = R.layout.activity_splash) : BaseActivity() {
     override fun initView() {
-        Handler().postDelayed(Runnable { startActivity(Intent(this, MainActivity::class.java)) }, 5000)
-        finish()
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()},
+            5000)
+
     }
 
     override fun addEvent() {
